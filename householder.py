0b0d4946950a33b7bd86467b49060f268ad97013
LucBoissieras <lucboissieras@gmail.com>
2017-03-15 12:00:06 +0100

    householder

--- householder.py
+++ householder.py
@@ -9,16 +9,16 @@ def householder(X, Y):
 
 def mul_householder(X, H):
     return H.dot(X)
-
+        
 def mul_householder_optimized(X, I, J):
     I = np.array([[3], [4], [0]])
     J = np.array([[0], [0], [5]])
     U = (I - J) / np.linalg.norm(I - J)
-    Z = X - 2 * U.dot(U.T.dot(X))
-    return Z
+    S = X - 2 * U.dot(U.T.dot(X))
+    return S
 
 I = np.array([[3], [4], [0]])
 J = np.array([[0], [0], [5]])
-X = np.array([[7], [9], [0]])
+X = np.array([[3, 4, 7], [4, 7, 9], [0, 0, 0]])
 print(mul_householder(X, householder(I, J)))
 print(mul_householder_optimized(X, I, J))
