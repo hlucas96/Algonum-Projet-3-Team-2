e3d2225ab0dd24a70d3e8c323ed42aa92e16fe24
LucBoissieras <lucboissieras@gmail.com>
2017-03-16 12:07:15 +0100

    householder with curves!

--- householder.py
+++ householder.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+import time
 
 def householder(X, Y):
     U = (X - Y) / np.linalg.norm(X - Y)
@@ -7,18 +8,41 @@ def householder(X, Y):
     H = I - 2.0 * U.dot(U.T)
     return H
 
-def mul_householder(X, H):
+def mul_householder(X, I, J):
+    H = householder(I, J)
     return H.dot(X)
         
 def mul_householder_optimized(X, I, J):
-    I = np.array([[3], [4], [0]])
-    J = np.array([[0], [0], [5]])
     U = (I - J) / np.linalg.norm(I - J)
     S = X - 2 * U.dot(U.T.dot(X))
     return S
 
+def plot_householder():
+    max_size = 500
+    xaxis = np.arange(1, max_size, 10)
+    execute_time_n = []
+    execute_time_o = []
+    I = np.zeros(max_size)
+    J = np.ones(max_size)
+    X = np.ones(max_size)
+    for h in xaxis:
+        t_a = time.time()
+        mul_householder(X[:h],I[:h], J[:h])
+        t_b = time.time()
+        mul_householder_optimized(X[:h],I[:h], J[:h])
+        t_c = time.time()
+        execute_time_n.append(t_b - t_a)
+        execute_time_o.append(t_c - t_b)
+
+        
+    l_h_n, = plt.plot(xaxis, execute_time_n, linewidth = 1.0)
+    l_h_o, = plt.plot(xaxis, execute_time_o, linewidth = 1.0)
+    plt.legend([l_h_n, l_h_o], ["householder", "householder_optimized"])
+    plt.show()
+    
 I = np.array([[3], [4], [0]])
 J = np.array([[0], [0], [5]])
 X = np.array([[3, 4, 7], [4, 7, 9], [0, 0, 0]])
-print(mul_householder(X, householder(I, J)))
+print(mul_householder(X, I, J))
 print(mul_householder_optimized(X, I, J))
+plot_householder()
