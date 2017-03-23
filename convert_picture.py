e5f0c986c69fdde6e1a8b508954e020ab2f81ea9
Hugo LUCAS <lucashugo96@hotmail.fr>
2017-03-23 16:26:46 +0100

    End of convert_picture

--- convert_picture.py
+++ convert_picture.py
@@ -1,8 +1,10 @@
-import pylab as pl
+import matplotlib.pyplot as pl
 import numpy as np
+import matplotlib.image as img
 
 def picture_to_matrix(path):
-    img_full = pl.imread(path)
+    ##Return 3 matrixs corresponding to the color Red Green Blue of the picture in path
+    img_full = img.imread(path)
     n = len(img_full);
     m = len(img_full[0])
     R,V,B = np.zeros((n,m)),np.zeros((n,m)),np.zeros((n,m))
@@ -13,6 +15,7 @@ def picture_to_matrix(path):
     return (R,V,B)
 
 def matrix_to_picture(R,V,B):
+    ##Print the picture corresponding to the colours of the matrix R, V, B
     n = len(R);
     m = len(R[0])
     M = np.zeros((n,m,3))
@@ -20,6 +23,8 @@ def matrix_to_picture(R,V,B):
         for j in range(m):
             M[i][j][0],M[i][j][1],M[i][j][2] = R[i][j],V[i][j],B[i][j]
     pl.imshow(M)
+    pl.show()
 
+#Exemple
 R,V,B = picture_to_matrix("essai.png")
 matrix_to_picture(R, V, B)
