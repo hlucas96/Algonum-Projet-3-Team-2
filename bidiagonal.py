4ebd3ba528b9b678fe269d5c2ecd2c7c233a38a6
LucBoissieras <lucboissieras@gmail.com>
2017-03-24 11:36:53 +0100

    now it's working!

--- bidiagonal.py
+++ bidiagonal.py
@@ -3,9 +3,6 @@ import householder as hh
 
 def implant(n, m, Q, i):
     I = np.identity(n)
-    print(Q)
-    print(m)
-    print(i)
     for k in range (i, n, 1):
         for l in range (i, m, 1):
             I[k][l] = Q[k - i][l - i]
@@ -19,13 +16,12 @@ def norme(X):
 
 def bi_diagonal(A):
     BD = A
-    n, m =A.shape
+    n, m = A.shape
     Qleft = np.identity(n)
     Qright = np.identity(n)
     for i in range (0, n - 1, 1):
         X = np.copy(BD[i:n, i])
         X.shape = (n - i, 1)
-        #X[0] = 0
         
         Y = np.zeros(n - i)
         Y[0] = norme(X)
@@ -38,22 +34,21 @@ def bi_diagonal(A):
         if (i != m - 2):
             X = np.copy(BD[i, (i + 1):m])
             X.shape = (m - i - 1, 1)
-            X[0] = 0
-            
+
             Y = np.zeros(m - i - 1)
             Y[0] = norme(X)
             Y.shape = (m - i - 1, 1)
             
             Q2_t = hh.householder(X, Y)
-            Q2 = implant(n, m - 1, Q2_t, i)
+            Q2 = implant(n, m, Q2_t, i + 1)
             Qright = Qright.dot(Q2)
             
             BD  = BD.dot(Q2)
-        print(np.round(BD, 2))
     return (Qleft, BD, Qright)
 
 
-A = np.array([[5, 3, 3], [3, 2, 3], [4, 2, 3]])
+A = np.array([[1, 2, 3], [4, 5, 9], [7, 8, 9]])
 Qleft, BD, Qright = bi_diagonal(A)
 print(A)
+print(np.round(BD, 2))
 print(Qleft.dot(BD).dot(Qright))
