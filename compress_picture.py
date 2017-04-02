5e92ec95611aa388e8fecb904b7d271ed6e653ea
hlucas96 <lucashugo96@hotmail.fr>
2017-04-02 16:39:42 +0200

    compress

--- compress_picture.py
+++ compress_picture.py
@@ -9,11 +9,12 @@ import bidiagonal as bi
 def compress(M, k):
     n = len(M)
     QL, BD, QR = bi.bi_diagonal(M, 0)
-    U, S, V = qr.transfo_QR(BD, n)
+    U, S, V = qr.transfo_USV(BD, 10**2)
+    U, S = qr.ordre_vp(U, S)
     if (k < n):
-        for i in range(k + 1, n + 1):
+        for i in range(k + 1, n):
             S[i][i] = 0
-    res = np.dot(QL,np.dot(U, np.dot(s, np.dot(V, QR))))
+    res = np.dot(QL,np.dot(U, np.dot(S, np.dot(V, QR))))
     return res
 
 ##Definition de la norme comme l'element maximal de la matrice
@@ -30,20 +31,23 @@ def norme(M):
 ##Trace l'efficacite en fonction du rang pour une matrice donnee
 def efficacite(M):
     n = len(M)
-    x = range(1, n + 1, 100)
+    QL, BD, QR = bi.bi_diagonal(M, 0)
+    U, S, V = qr.transfo_USV(BD, 10**2)
+    U, S = qr.ordre_vp(U, S)
+    x = range(n-1, 0, -1)
     distances = []
-    for k in x:
-        M_comp = compress(M, k)
-        print("done")
-        distances += norme(M - M_comp)
-    pl.plot(x,distances)
+    for i in x:
+        S[i][i] = 0
+        distances += [norme(M - M_comp)]
+    pl.plot(mirror(x), mirror(distances))
     pl.xlabel("Rang de compression k")
     pl.ylabel("Efficacite de la compression")
     pl.show()
 
 
-R, V, B = conv.picture_to_matrix("essai2.png")
-print(qr.transfo_QR(R, 500))
+R, V, B = conv.picture_to_matrix("essai.png")
+k = 25
+conv.matrix_to_picture(compress(R, k), compress(V, k), compress(B, k))
 efficacite(R)
-for k in [1, 225, 375, 500]:
-    conv.matrix_to_picture(compress(R, k), compress(V, k), compress(B, k))
+#for k in [1, 50, 75, 100]:
+#    conv.matrix_to_picture(compress(R, k), compress(V, k), compress(B, k))
